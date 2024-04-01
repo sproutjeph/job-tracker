8e1bd3471df0e104f34bb41b9dd5779c3e97f2a6
jephthah <jephthah.mbah@outlook.com>
2024-04-01 15:19:20 +0100

    added login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,13 +1,33 @@
-import { Link } from "react-router-dom";
-import { FC } from "react";
+import SubmitButton from "@/components/SubmitButton";
+import { Button } from "@/components/ui/button";
+import { Link, Form } from "react-router-dom";
+import { Card } from "@/components/ui/card";
+import FormRow from "@/components/FormRow";
+import Logo from "@/components/Logo";
 
-interface LoginProps {}
-
-const Login: FC<LoginProps> = () => {
+const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to="/register">Register Page</Link>
+    <div className="h-screen flex justify-center items-center">
+      <Card className="w-full max-w-md mx-auto p-8 border-t-4  border-t-primary justify-center items-center flex flex-col shadow-md">
+        <Logo />
+        <h1 className="my-4 text-xl tracking-wider">Register</h1>
+
+        <Form className="w-full space-y-6" method="post">
+          <FormRow labelText="Email" name="name" type="text" />
+          <FormRow labelText="Password" name="password" type="password" />
+
+          <SubmitButton />
+          <Button className="w-full" variant="secondary">
+            Explore The App
+          </Button>
+          <div className="flex items-center gap-1 justify-center">
+            <p>Already have an account?</p>
+            <Link to="/register" className="text-primary">
+              Register
+            </Link>
+          </div>
+        </Form>
+      </Card>
     </div>
   );
 };
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,10 +1,32 @@
-import { Link } from "react-router-dom";
+import SubmitButton from "@/components/SubmitButton";
+import { Link, Form } from "react-router-dom";
+import { Card } from "@/components/ui/card";
+import FormRow from "@/components/FormRow";
+import Logo from "@/components/Logo";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/Login">Login Page</Link>
+    <div className="h-screen flex justify-center items-center">
+      <Card className="w-full max-w-md mx-auto p-8 border-t-4  border-t-primary justify-center items-center flex flex-col shadow-md">
+        <Logo />
+        <h1 className="my-4 text-xl tracking-wider">Register</h1>
+
+        <Form className="w-full space-y-6" method="post">
+          <FormRow labelText="Name" name="name" type="text" />
+          <FormRow labelText="Last Name" name="lasNname" type="text" />
+          <FormRow labelText="Location" name="location" type="text" />
+          <FormRow labelText="Email" name="email" type="email" />
+          <FormRow labelText="Password" name="password" type="password" />
+
+          <SubmitButton />
+          <div className="flex items-center gap-1 justify-center">
+            <p>Already have an account?</p>
+            <Link to="/login" className="text-primary">
+              Login
+            </Link>
+          </div>
+        </Form>
+      </Card>
     </div>
   );
 };
